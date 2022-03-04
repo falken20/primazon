f63406926fb6523f74a654692f3801c9031c60eb
Richi <r.rodriguez@bbva.com>
2022-03-04 08:17:46 +0100

    init_db finished

--- src/init_db.py
+++ src/init_db.py
@@ -1,53 +1,75 @@
+# by Richi Rod AKA @richionline / falken20
+
+# Create user and grant privileges, it should be done before in the terminal, important the ';' at the end
+# CREATE ROLE user LOGIN PASSWORD password;
+# GRANT ALL PRIVILEGES ON DATABASE db TO user;
+# GRANT CONNECT ON DATABASE my_db TO my_user;
+
 import os
 import psycopg2
 from dotenv import load_dotenv, find_dotenv
-from rich import print
+from rich.console import Console
 
 # Load .env file
 load_dotenv(find_dotenv())
 
-print("Connecting with [bold]DB[/bold]...", style="blue")
-conn = psycopg2.connect(
-    host="localhost",
-    database=os.environ['DB_DATABASE'],
-    user=os.environ['DB_USERNAME'],
-    password=os.environ['DB_PASSWORD'])
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
+def main():
+    console = Console()
+    console.print("[bold green]Process starting...[/bold green]")
 
-# Create user and grant privileges
-# cur.execute('CREATE USER %s WITH PASSWORD %s;', (user, password))
-# cur.execute('GRANT ALL PRIVILEGES ON DATABASE %s TO %s;', (database, user))
+    try:
+        console.print("Connecting with [bold]DB[/bold]...", style="blue")
+        conn = psycopg2.connect(
+            host="localhost",
+            database=os.environ['DB_DATABASE'],
+            user=os.environ['DB_USERNAME'],
+            password=os.environ['DB_PASSWORD'])
 
-# Execute a command: this creates a new table
-print("Create table [bold]t_products[/bold] if not exist...", style="blue")
-cur.execute('DROP TABLE IF EXISTS t_products;')
-cur.execute('CREATE TABLE t_products '
-            '(product_id serial PRIMARY KEY,'
-            'product_url varchar (500) NOT NULL,'
-            'product_desc varchar (150) NOT NULL,'
-            'product_price float NOT NULL,'
-            'product_date_added date DEFAULT CURRENT_TIMESTAMP,'
-            'product_date_updated date);'
-            )
+        # Open a cursor to perform database operations
+        cur = conn.cursor()
 
-print("Create table [bold]t_prices[/bold] if not exist...", style="blue")
-cur.execute('DROP TABLE IF EXISTS t_prices;')
-cur.execute('CREATE TABLE t_prices '
-            '(price_id serial PRIMARY KEY,'
-            'product_id varchar (500) NOT NULL,'
-            'product_price float NOT NULL,'
-            'product_date_added date DEFAULT CURRENT_TIMESTAMP,'
-            'CONSTRAINT fk_products'
-            '   FOREIGN KEY(product_id)'
-	        '   REFERENCES t_products(product_id))'
-            )
+        # Ask if we want to drp the tables if they exist
+        if input("Could you drop the tables if they exist (y/n)? ") in ["Y", "y"]:
+            console.print("Drop table [bold]t_prices[/bold]...", style="blue")
+            cur.execute('DROP TABLE IF EXISTS t_prices;')
+            console.print("Drop table [bold]t_products[/bold]...", style="blue")
+            cur.execute('DROP TABLE IF EXISTS t_products;')
 
-conn.commit()
+        # Execute a command: this creates a new table
+        console.print("Create table [bold]t_products[/bold]...", style="blue")
+        cur.execute('CREATE TABLE t_products '
+                    '(product_id serial PRIMARY KEY,'
+                    'product_url varchar (500) NOT NULL,'
+                    'product_desc varchar (150) NOT NULL,'
+                    'product_price float NOT NULL,'
+                    'product_date_added date DEFAULT CURRENT_TIMESTAMP,'
+                    'product_date_updated date);'
+                    )
 
-print("Closing connection with [bold]DB[/bold]...", style="blue")
-cur.close()
-conn.close()
-print("Process finished [bold]succesfully[/bold]", style="blue")
+        console.print("Create table [bold]t_prices[/bold]...", style="blue")
+        cur.execute('CREATE TABLE t_prices '
+                    '(price_id serial PRIMARY KEY,'
+                    'product_id serial,'
+                    'product_price float NOT NULL,'
+                    'product_date_added date DEFAULT CURRENT_TIMESTAMP,'
+                    'CONSTRAINT fk_products'
+                    '   FOREIGN KEY(product_id)'
+                    '   REFERENCES t_products(product_id))'
+                    )
 
+        conn.commit()
+
+        console.print(
+            "Closing connection with [bold]DB[/bold]...", style="blue")
+        cur.close()
+        conn.close()
+        console.print("[bold green]Process finished succesfully[/bold green]")
+
+    except Exception as err:
+        console.print(f"[red bold]EXECUTION ERROR: {format(err)}")
+        return False
+
+
+if __name__ == "__main__":
+    main()
