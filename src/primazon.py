f9f779b296ff6970860eaaa17977f8f777db9bf1
Richi <r.rodriguez@bbva.com>
2022-03-04 23:49:24 +0100

    Adding privileges on tables and products CRUD screen

--- src/primazon.py
+++ src/primazon.py
@@ -1,14 +1,33 @@
 # by Richi Rod AKA @richionline / falken20
 
-from flask import Flask, render_template, url_for
 import os
+from flask import Flask, render_template, url_for
+import psycopg2
 
-app = Flask(__name__, template_folder='../docs/templates', static_folder='../docs/static')
+from . import init_db
+
+
+app = Flask(__name__, template_folder='../docs/templates',
+            static_folder='../docs/static')
 # Set this var to True to be able to make any web change and take the changes with refresh
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.route('/')
 def index():
-    # url_for('docs/static/css', filename='static.css')
-    return render_template('index.html')
+    """
+    Show the screen with products
+
+    Returns:
+        str: HTML page for products
+    """
+
+    # Get the connection to the database
+    conn = init_db.get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM t_products;')
+    products = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return render_template('index.html', products=products)
