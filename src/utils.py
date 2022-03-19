ec07bcb5bb58475d263aebadeacb8ad3a85d115c
Richi <r.rodriguez@bbva.com>
2022-03-20 00:11:50 +0100

    Scrapping test with selectorlib and beatifulsoup4

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,32 @@
 # by Richi Rod AKA @richionline / falken20
 import sys
-import pandas as pd
+import os
+from click import style
+import requests
+
 from rich.console import Console
+from selectorlib import Extractor
+from bs4 import BeautifulSoup
 
 # Load .env file
 # load_dotenv(find_dotenv())
 
 # Create console object
 console = Console()
+
+headers = {
+    'authority': 'www.amazon.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}
 
 
 def scrap_web(url):
@@ -18,10 +37,17 @@ def scrap_web(url):
         url (str): url web to scrap
     """
     try:
-        url = "https://www.amazon.es/gp/product/B09JR6YL4B/ref=ox_sc_saved_title_6?smid=A1AT7YVPFBWXBL&psc=1"
-        console.print(f"Starting process to scrap web: \n{url}", style="bold green")
-        data = pd.read_html(url)
-        print(data)
+        url = "https://www.amazon.es/gp/product/B09JR6YL4B"
+
+        page = requests.get(url, headers=headers)
+
+        soup = BeautifulSoup(page.content, "html.parser")
+        print(soup.find(id="productTitle").text.strip())  # By DOM element id
+        # By DOM element class
+        print(soup.find(class_="a-offscreen").text.strip())
+
+        extractor = Extractor.from_yaml_file(os.path.join(os.path.dirname(__file__), 'selectors.yml'))
+        print(extractor.extract(page.text))
 
         console.print("[bold green]Process finished succesfully[/bold green]")
 
@@ -34,6 +60,15 @@ def scrap_web(url):
 
 
 if __name__ == "__main__":
-    url = str(sys.argv[1]) if len(sys.argv) > 1 else ""
-    scrap_web(url)
+    console.print(f"Starting primazon utils...", style="bold green")
+    console.print("Select option:", style="yellow")
+    console.print("1. Scrap url", style="yellow")
+    console.print("2. Exit", style="yellow")
+    option = input()
+    if option == "1":
+        url = str(sys.argv[1]) if len(sys.argv) > 1 else None
+        console.print("Please entry the url to scrap:", style="yellow")
+        url = input() if not url else ""
+        scrap_web(url)
 
+    console.print(f"Primazon utils finished", style="bold green")
