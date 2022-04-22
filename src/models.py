efeabff8f565f36f04c1e14a1104a73c105cba06
Richi <r.rodriguez@bbva.com>
2022-04-22 08:35:55 +0200

    Process to create tables improved

--- src/models.py
+++ src/models.py
@@ -5,9 +5,9 @@
 
 import datetime
 from rich.console import Console
-from sqlalchemy import engine
+from sqlalchemy.sql import func
 
-from .primazon import db
+from src.primazon import db
 
 # Create console object for logs
 console = Console()
@@ -15,7 +15,7 @@ console = Console()
 
 class Product(db.Model):
     # Table name in class name in camel_case. You can override with __tablename__
-    __tablename__ = "products"
+    __tablename__ = "t_products"
 
     product_id = db.Column(db.Integer, primary_key=True)
     product_url = db.Column(db.String(500), nullable=False)
@@ -26,41 +26,60 @@ class Product(db.Model):
     product_reviews = db.Column(db.String(100), nullable=True)
     product_min_price = db.Column(db.Float)
     product_max_price = db.Column(db.Float)
-    product_date_added = db.Column(db.Date, default=datetime.date.today)
+    product_date_added = db.Column(db.Date, default=func.now())
     product_date_update = db.Column(db.Date)
+
+    product_prices = db.relationship('Price')
 
     def __repr__(self) -> str:
         return f"Product: {self.product_desc}"
 
 
 class Price(db.Model):
-    __tablename__ = "prices"
+    __tablename__ = "t_prices"
 
     price_id = db.Column(db.Integer, primary_key=True)
     product_price = db.Column(db.Float)
-    price_date_added = db.Column(db.Date, default=datetime.date.today)
+    price_date_added = db.Column(db.Date, default=func.now())
 
     product_id = db.Column(
         db.Integer,
-        db.ForeignKey('products.product_id', ondelete='CASCADE'),
+        db.ForeignKey('t_products.product_id', ondelete='CASCADE'),
         nullable=False
     )
 
     product = db.relationship(
-        'Product', backref=db.backref('prices', order_by=price_id), lazy=True)
+        'Product', backref=db.backref('t_prices', order_by=price_id), lazy=True)
 
     def __repr__(self) -> str:
         return f"Product price: {self.product_id} - {self.product_price}"
 
 
 def init_db():
-    console.print("Creating Database if doesn't exist", style="blue")
-    db.create_all()
-    db.session.commit()
+    """
+    Main process to create the needed tables for the application
+    """
+    console.print("Init DB process starting...", style="bold green")
 
-    from sqlalchemy.ext.declarative import declarative_base
-    Base = declarative_base()
-    Base.metadata.create_all(bind=engine)
+    try:
+        if input("Could you drop the tables if they exist(y/n)? ") in ["Y", "y"]:
+            db.drop_all()
+            console.print("Tables dropped", style="blue")
+
+        if input("Could you create the tables(y/n)? ") in ["Y", "y"]:
+            console.print("Creating tables...", style="blue")
+            db.create_all()
+            
+        db.session.commit()
+
+        console.print("Process finished succesfully", style="bold green")
+
+    except Exception as err:
+        console.print(
+            "Execution Error in init_db:" +
+            f"\nLine {sys.exc_info()[2].tb_lineno} {type(err).__name__} " +
+            f"\nFile: {sys.exc_info()[2].tb_frame.f_code.co_filename} " +
+            f"\n{format(err)}", style="red bold")
 
 
 if __name__ == '__main__':
