6a4b5fd89f27f8786bff5e4dff400362007dbae5
Richi <r.rodriguez@bbva.com>
2022-05-24 08:47:47 +0200

    Styles and error types

--- src/utils_logs.py
+++ src/utils_logs.py
@@ -1,18 +1,20 @@
-import logging
-from rich.console import Console
 import sys
 import os
+from rich.console import Console
 from datetime import datetime
 
+# Create console object for logs
 console = Console()
 
 COLOR_DEBUG = "blue"
 COLOR_INFO = "green"
-COLOR_WARNING = "orange bold"
-COLOR_ERROR = "red bold"
+COLOR_WARNING = "orange3 bold"
+COLOR_ERROR = "red bold on white"
+COLOR_UNKNOW = "pink"
 
 # Min log level to print
-LEVEL=os.environ["LEVEL"]
+LEVEL_LOG = os.environ["LEVEL_LOG"]
+
 
 def loggear(message="", level="INFO", err=Exception, sys=sys):
     color = eval("COLOR_" + level)
@@ -23,12 +25,17 @@ def loggear(message="", level="INFO", err=Exception, sys=sys):
                       f"\nLine: {sys.exc_info()[2].tb_lineno} {type(err).__name__} ",
                       f"\nMethod: {sys.exc_info()[2].tb_frame.f_code.co_name} ",
                       f"\nFile: {sys.exc_info()[2].tb_frame.f_code.co_filename} ",
-                      f"\nError: {format(err)}", style=color)
-    else:
+                      f"\nError: {format(err)}",
+                      style=color)
+    elif level in LEVEL_LOG:
         console.print(time, level, message, style=color)
+    else:
+        console.print(time, level, message, style=COLOR_UNKNOW)
 
 
 """
+import logging
+
 FORMAT = '%(asctime)s %(levelname)s %(lineno)d %(filename)s %(funcName)s: %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 
