881ccbb60cccb6daf4f13850abb9d746adc128f4
Richi <r.rodriguez@bbva.com>
2022-05-25 16:39:51 +0200

    Change calls to print log for loggear

--- src/utils_logs.py
+++ src/utils_logs.py
@@ -1,37 +1,38 @@
 import sys
 import os
 from rich.console import Console
+from rich.style import Style
 from datetime import datetime
 
 # Create console object for logs
 console = Console()
 
-COLOR_DEBUG = "blue"
-COLOR_INFO = "green"
-COLOR_WARNING = "orange3 bold"
-COLOR_ERROR = "red bold on white"
-COLOR_UNKNOW = "pink"
+style_DEBUG = Style(color="cyan")  # style_debug = "red bold"
+style_INFO = Style(color="green")
+style_WARNING = Style(color="orange3", bold=True)
+style_ERROR = Style(color="red", bgcolor="white", bold=True)
 
 # Min log level to print
 LEVEL_LOG = os.environ["LEVEL_LOG"]
 
 
 def loggear(message="", level="INFO", err=Exception, sys=sys):
-    color = eval("COLOR_" + level)
-    time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    try:
+        time = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        color = eval("style_" + level)
 
-    if level in ["ERROR"]:
-        console.print(time, level, message,
-                      f"\nLine: {sys.exc_info()[2].tb_lineno} {type(err).__name__} ",
-                      f"\nMethod: {sys.exc_info()[2].tb_frame.f_code.co_name} ",
-                      f"\nFile: {sys.exc_info()[2].tb_frame.f_code.co_filename} ",
-                      f"\nError: {format(err)}",
-                      style=color)
-    elif level in LEVEL_LOG:
-        console.print(time, level, message, style=color)
-    else:
-        console.print(time, level, message, style=COLOR_UNKNOW)
+        if level in ["ERROR"]:
+            console.print(time, level, message,
+                        f"\nLine: {sys.exc_info()[2].tb_lineno} {type(err).__name__} ",
+                        f"\nMethod: {sys.exc_info()[2].tb_frame.f_code.co_name} ",
+                        f"\nFile: {sys.exc_info()[2].tb_frame.f_code.co_filename} ",
+                        f"\nError: {format(err)}",
+                        style=color)
+        elif level in LEVEL_LOG:
+            console.print(time, level, message, style=color)
 
+    except Exception as err:
+        console.print(time, level, message, f"\nError: {format(err)}", style=style_ERROR)
 
 """
 import logging
