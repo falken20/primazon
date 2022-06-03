9c6bd104a144691cbd72ad5cb52192dee53bbb61
Richi <r.rodriguez@bbva.com>
2022-06-03 07:28:47 +0200

    Firts tests

---
+++ tests/test_models.py
@@ -0,0 +1,9 @@
+import unittest
+from src import models
+
+class TestProduct(unittest.TestCase):
+    def setUp(self) -> None:
+        self.product = models.Product()
+
+    def test_print_product(self):
+        self.assertIn("ID", print(self.product))
