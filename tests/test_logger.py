355ef6f3716c43a2a2b6b668e6b7739d1b0a2924
Richi <r.rodriguez@bbva.com>
2022-06-10 08:46:27 +0200

    Test for different print log methods in logger module

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,15 +1,104 @@
 import os
+from io import StringIO
+import sys
 from src import logger
+import pytest
+
+
+def redirect_stdout():
+    captured_output = StringIO()  # Create StringIO object
+    sys.stdout = captured_output  # and redirect stdout
+    return captured_output
+
+
+def redirect_reset():
+    sys.stdout = sys.__stdout__  # Reset redirect
 
 
 def test_debug():
-    os.environ["LEVE_LOG"] = "['DEBUG']"
-    ret = logger.Log.debug("Test Debug")
-    assert "Test Debug" in ret
+    logger.LEVEL_LOG = "['DEBUG']"
+    trace = "Test Debug"
+
+    captured_output = redirect_stdout()
+    logger.Log.debug(trace)
+    redirect_reset()
+
+    assert trace in captured_output.getvalue()
 
 
 def test_debug_no_trace():
-    os.environ["LEVE_LOG"] = "[]"
-    ret = logger.Log.debug("Test debug") 
-    assert ret is None
+    logger.LEVEL_LOG = "[]"
+    trace = "Test Debug"
 
+    captured_output = redirect_stdout()
+    logger.Log.debug(trace)
+    redirect_reset()
+
+    assert trace not in captured_output.getvalue()
+
+
+def test_info():
+    logger.LEVEL_LOG = "['INFO']"
+    trace = "Test Info"
+
+    captured_output = redirect_stdout()
+    logger.Log.info(trace)
+    redirect_reset()
+
+    assert trace in captured_output.getvalue()
+
+
+def test_info_no_trace():
+    logger.LEVEL_LOG = "[]"
+    trace = "Test Info"
+
+    captured_output = redirect_stdout()
+    logger.Log.info(trace)
+    redirect_reset()
+
+    assert trace not in captured_output.getvalue()
+
+
+def test_warning():
+    logger.LEVEL_LOG = "['WARNING']"
+    trace = "Test Warning"
+
+    captured_output = redirect_stdout()
+    logger.Log.warning(trace)
+    redirect_reset()
+
+    assert trace in captured_output.getvalue()
+
+
+def test_warning_no_trace():
+    logger.LEVEL_LOG = "[]"
+    trace = "Test Warning"
+
+    captured_output = redirect_stdout()
+    logger.Log.warning(trace)
+    redirect_reset()
+
+    assert trace not in captured_output.getvalue()
+
+
+def test_error():
+    logger.LEVEL_LOG = "['ERROR']"
+    trace = "Test Error"
+    err = pytest.raises(Exception)
+
+    captured_output = redirect_stdout()
+    logger.Log.error(trace, err, sys)
+    redirect_reset()
+
+    assert trace in captured_output.getvalue()
+
+
+def test_error_no_trace():
+    logger.LEVEL_LOG = "[]"
+    trace = "Test Error"
+
+    captured_output = redirect_stdout()
+    logger.Log.error(trace)
+    redirect_reset()
+
+    assert trace not in captured_output.getvalue()
