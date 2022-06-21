db028287dfee71569ea48d5c42db120e01a68f4e
Richi <r.rodriguez@bbva.com>
2022-06-21 08:51:02 +0200

    Preparing setUp and tearDown to test PostgreSQL

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 from src.models import Product, db
 
-test_product = {"product_url": "url", "product_price": 1}
+TEST_PRODUCT = {"product_url": "url", "product_price": 1}
 
 class TestProduct(unittest.TestCase):
 
@@ -34,14 +34,38 @@ class TestProduct(unittest.TestCase):
             db.session.remove()
             db.drop_all()
 
-    def test_product(self):
-        product = Product.create_product(test_product)
-        print(product)
+    def test_print_product(self):
+        product = Product.create_product(TEST_PRODUCT)
+        db.session.add(product)
+        db.session.commit()
+
+        print("******", product.__repr__)
+        self.assertIn ("ID:", product.__repr__)
+
+    def test_create_product(self):
+        product = Product.create_product(TEST_PRODUCT)
         db.session.add(product)
         db.session.commit()
 
         assert product in db.session
 
+    
+    def test_get_product(self):
+        product = Product.create_product(TEST_PRODUCT)
+        db.session.add(product)
+        db.session.commit()
+
+        assert product == Product.get_product(product.product_id)
+
+
+    def test_get_all_products(self):
+        product = Product.create_product(TEST_PRODUCT)
+        db.session.add(product)
+        product = Product.create_product(TEST_PRODUCT)
+        db.session.add(product)
+        db.session.commit()
+
+        assert 2 == len(Product.get_all_products())
 
 """
     def test_print_product(self):
