2275cb564d0006285e2d086b00cd5bb801c61497
Richi <r.rodriguez@bbva.com>
2023-09-04 08:58:46 +0200

    Refactoring email methods

---
+++ src/mail_config.py
@@ -0,0 +1,33 @@
+# by Richi Rod AKA @richionline / falken20
+
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"
+receiver_email = "ricardorg20@gmail.com"
+password = input("Type your password and press enter (SMTP_SSL() demo): ")
+filename = "document.pdf"  # In same directory as script
+
+"""The message string starts with "Subject: Hi there" followed by two newlines (\n).
+This ensures Hi there shows up as the subject of the email, and the text following the
+newlines will be treated as the message body"""
+message = """\
+Subject: Hi
+
+This message is sent from Python..."""
+
+# Create the plain-text and HTML version of your message
+message_plaintext = """\
+Hi,
+How are you?
+Real Python has many great tutorials:
+www.realpython.com"""
+message_html = """\
+<html>
+  <body>
+    <p>Hi,<br>
+       How are you?<br>
+       <a href="http://www.realpython.com">Real Python</a>
+       has many great tutorials.
+    </p>
+  </body>
+</html>
+"""
