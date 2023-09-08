49b9013165c6d440bc94b605477492e800c087c2
Richi <r.rodriguez@bbva.com>
2023-09-08 08:14:17 +0200

    Refactoring code

--- src/mail.py
+++ src/mail.py
@@ -22,7 +22,7 @@ from logger import Log
 # For testing with local smtp you have to use localhost as smtp_server and port 1025 rather than
 # port 465 (SMTO_SSL) or 587  (starttls)
 
-LOCAL_TESTING = True
+LOCAL_TESTING = False
 LOCAL_SMTP_SERVER = "localhost"
 LOCAL_PORT = 1025
 
@@ -71,66 +71,74 @@ def send_email_starttls(smtp_server: str, sender_email: str, password: str, rece
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
-    except Exception as e:
-        # Print any error messages to stdout
-        print(e)
+    except Exception as err:
+        Log.error("Error sending email with starttls: ", err, sys)
     finally:
-        server.quit()
+        try:
+            server.quit()
+        except Exception as err:
+            Log.error("Error closing server: ", err, sys)
 
 
 # Email in HTML and plain-text versions ########################################
 def send_email_HTML_plaintext(smtp_server: str, sender_email: str, password: str,
                               receiver_email: str, message_plaintext: str, message_html: str):
-    message = MIMEMultipart("alternative")
-    message["Subject"] = "multipart test"
-    message["From"] = sender_email
-    message["To"] = receiver_email
+    try:
+        message = MIMEMultipart("alternative")
+        message["Subject"] = "multipart test"
+        message["From"] = sender_email
+        message["To"] = receiver_email
 
-    # Turn versions plain-text and HTML into MIMEText objects
-    part_plain = MIMEText(message_plaintext, "plain")
-    part_html = MIMEText(message_html, "html")
+        # Turn versions plain-text and HTML into MIMEText objects
+        part_plain = MIMEText(message_plaintext, "plain")
+        part_html = MIMEText(message_html, "html")
 
-    # Add parts to MIMEMultipart message
-    # IMPORTANT: The email client will try to render the last part first
-    message.attach(part_plain)
-    # IMPORTANT: The email client will try to render the last part first
-    message.attach(part_html)
+        # Add parts to MIMEMultipart message
+        # IMPORTANT: The email client will try to render the last part first
+        message.attach(part_plain)
+        # IMPORTANT: The email client will try to render the last part first
+        message.attach(part_html)
 
-    send_email_SMTP_SSL(smtp_server, sender_email, password,
-                        receiver_email, message.as_string())
+        send_email_SMTP_SSL(smtp_server, sender_email, password,
+                            receiver_email, message.as_string())
+    except Exception as err:
+        Log.error("Error sending HTML/plaintext email: ", err, sys)
 
 
 # Email with attachment file ########################################
 def send_email_attachment(smtp_server: str, sender_email: str, password: str,
                           receiver_email: str, filename: str, message: str):
-    message = MIMEMultipart()
-    message["From"] = sender_email
-    message["To"] = receiver_email
-    message["Subject"] = "An email with attachment from Python"
-    message.attach(MIMEText(message, "plain"))
+    try:
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = "An email with attachment from Python"
+        message.attach(MIMEText(message, "plain"))
 
-    # Open PDF file in binary mode
-    with open(filename, "rb") as attachment:
-        # Add file as application/octet-stream
-        # Email client can usually download this automatically as attachment
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
+        # Open PDF file in binary mode
+        with open(filename, "rb") as attachment:
+            # Add file as application/octet-stream
+            # Email client can usually download this automatically as attachment
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
 
-    # Encode file in ASCII characters to send by email
-    encoders.encode_base64(part)
+        # Encode file in ASCII characters to send by email
+        encoders.encode_base64(part)
 
-    # Add header as key/value pair to attachment part
-    part.add_header(
-        "Content-Disposition",
-        f"attachment; filename= {filename}",
-    )
+        # Add header as key/value pair to attachment part
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename= {filename}",
+        )
 
-    # Add attachment to message and convert message to string
-    message.attach(part)
-    text = message.as_string()
+        # Add attachment to message and convert message to string
+        message.attach(part)
+        text = message.as_string()
 
-    send_email_SMTP_SSL(smtp_server, sender_email,
-                        password, receiver_email, text)
+        send_email_SMTP_SSL(smtp_server, sender_email,
+                            password, receiver_email, text)
+    except Exception as err:
+        Log.error("Error sending email with attachment: ", err, sys)
 
 
 if __name__ == "__main__":
@@ -139,7 +147,8 @@ if __name__ == "__main__":
     try:
         password = None
         if LOCAL_TESTING:
-            Log.info("Testing with local SMTP server...")
+            # Before starting command: $ python -m smtpd -c DebuggingServer -n localhost:1025
+            Log.info("0. Testing with local SMTP server...")
             send_email_local_smtp(LOCAL_SMTP_SERVER, LOCAL_PORT, mail_config.sender_email,
                                   mail_config.receiver_email, "Testing local SMTP")
             exit()
